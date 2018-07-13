fdfb7d377d3558789354b78ecfa7f5549940cfb0
paasoms <oms@hermes-sys.com>
2018-07-12 18:50:53 -0700

    remove background.gif

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,4 +2,20 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Question
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdamin):
+
+    list_display = ('question_text','pub_date','was_published_recently')
+
+    fieldsets = [
+                   (None,               {'fields':['question_text']}),
+                   ('Date information', {'fields':['pub_date']}),
+                ]   
+
+admin.site.register(Question,QuestionAdmin)
+admin.site.register(Choice)
+
